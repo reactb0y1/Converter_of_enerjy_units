26e79e3b540cab1248c33d411ee18d7de33b7351
sashakobzeww <sashakobzeww@gmail.com>
2017-11-18 13:04:06 +0200

    Учимся получать данные из input

---
+++ common.js
@@ -0,0 +1,14 @@
+function plus() {
+
+	var result, num1, num2;
+
+	num1 = $("#num1").value();
+	num2 = $("#num2").value();
+	console.log(num1);
+
+/*	result = num1 + num2;
+
+	console.log(result);
+
+	$("#result").text(result);
+*/}

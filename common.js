b4cb1dee7797d45b984cf4939ce066a09c15921f
sashakobzeww <sashakobzeww@gmail.com>
2017-11-18 13:15:04 +0200

    Калькулятор, умеющий суммировать

--- common.js
+++ common.js
@@ -1,14 +1,18 @@
 function plus() {
 
-	var result, num1, num2;
+	var result = 0;
+	var num1 = 0;
+	var num2 = 0;
 
-	num1 = $("#num1").value();
-	num2 = $("#num2").value();
+	// num1 = $("#num1").value;
+	// num2 = $("#num2").value;
+	num1 = document.getElementById('num1').value;
+	num2 = document.getElementById('num2').value;
 	console.log(num1);
 
-/*	result = num1 + num2;
+	result = +num1 + +num2;
 
 	console.log(result);
 
 	$("#result").text(result);
-*/}
+}
